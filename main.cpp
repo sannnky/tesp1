5395eb447102571f8b4f9e8e043661c1e406d68b
sannnky <mohammadichsannurdin@gmail.com>
2023-10-17 16:51:28 +0700

    menambahkan file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout << "------------------------------------------------------------------------------------------------------------------------" << endl;
+    cout << "Nama : Mohammad Ichsan Nurdin\n\n";
+    cout << "NPM : 2310631170097" << endl << endl;
+    cout << "Kelas : A - Informatika" << endl << endl;
+    cout << "Cita-cita : Bingung si sekarang" << endl << endl;
+    cout << "------------------------------------------------------------------------------------------------------------------------";
+    return 0;
+}
